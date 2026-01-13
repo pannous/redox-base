9427cb51c9137f619a73c241ebd23f866fd4b0c7
pannous <info@pannous.com>
2026-01-13 10:34:44 +0100

    wip: auto-save after Edit

---
+++ dns-test/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CRANELIFT="/opt/other/redox/rust/compiler/rustc_codegen_cranelift/target/release/librustc_codegen_cranelift.dylib"
+TARGET="aarch64-unknown-redox-clif.json"
+SYSROOT="/opt/other/redox/build/aarch64/sysroot/lib"
+
+cp /opt/other/redox/tools/$TARGET .
+
+RUSTFLAGS="-Zcodegen-backend=${CRANELIFT} -Crelocation-model=static -Clto=no -Clink-arg=-L${SYSROOT} -Clink-arg=-z -Clink-arg=muldefs -Cpanic=abort" \
+cargo +nightly build --target $TARGET --release \
+  -Zbuild-std=core,alloc,std,panic_abort
+
+cp target/aarch64-unknown-redox-clif/release/dns-test /opt/other/redox/share/
+ls -lh /opt/other/redox/share/dns-test

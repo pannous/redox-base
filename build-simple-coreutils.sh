8538d900a2c3e9e03fb33efde9decf7c32d8545c
pannous <info@pannous.com>
2026-01-15 17:45:16 +0100

    chore: fix build script paths and add test-mtime utility
    - Fix build-simple-coreutils.sh to use local crt*.o files
    - Add test-mtime utility for debugging time-based sorting issues

--- build-simple-coreutils.sh
+++ build-simple-coreutils.sh
@@ -8,16 +8,17 @@ NIGHTLY="nightly-2026-01-02"
 TARGET="aarch64-unknown-redox-clif.json"
 CRANELIFT="/opt/other/rustc_codegen_cranelift/dist/lib/librustc_codegen_cranelift.dylib"
 RELIBC="/opt/other/redox/recipes/core/relibc/source/target/aarch64-unknown-redox-clif/release"
+BASE_SOURCE="$(dirname "$0")"
 
 export DYLD_LIBRARY_PATH=~/.rustup/toolchains/${NIGHTLY}-aarch64-apple-darwin/lib
 
 export RUSTFLAGS="-Zcodegen-backend=${CRANELIFT} \
   -Crelocation-model=static \
   -Clink-arg=-L${RELIBC} \
-  -Clink-arg=${RELIBC}/crt0.o \
-  -Clink-arg=${RELIBC}/crt0_rust.o \
-  -Clink-arg=${RELIBC}/crti.o \
-  -Clink-arg=${RELIBC}/crtn.o \
+  -Clink-arg=${BASE_SOURCE}/crt0.o \
+  -Clink-arg=${BASE_SOURCE}/crt0_rust.o \
+  -Clink-arg=${BASE_SOURCE}/crti.o \
+  -Clink-arg=${BASE_SOURCE}/crtn.o \
   -Clink-arg=-lunwind_stubs \
   -Clink-arg=-z -Clink-arg=muldefs \
   -Cpanic=abort"
@@ -33,7 +34,7 @@ cargo +${NIGHTLY} build \
 echo "=== Stripping ==="
 OUT=/tmp/simple-coreutils
 mkdir -p $OUT
-for bin in simple-cat simple-rm simple-mkdir simple-echo simple-cp simple-touch; do
+for bin in simple-cat simple-rm simple-mkdir simple-echo simple-cp simple-touch test-mtime; do
     if [[ -f "target/aarch64-unknown-redox-clif/release/$bin" ]]; then
         llvm-strip -o "$OUT/$bin" "target/aarch64-unknown-redox-clif/release/$bin"
         echo "  $bin -> $OUT/$bin"
